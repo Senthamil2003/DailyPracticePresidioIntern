f8dc7700674474a55016a4e67b9044842643057b
Senthamil2003 <77163996+Senthamil2003@users.noreply.github.com>
2024-05-08 23:13:55 +0530

    day 19 4th query update

--- Day-19(CTE and proc )/Assignment- day-19.sql
+++ Day-19(CTE and proc )/Assignment- day-19.sql
@@ -40,11 +40,55 @@ select au_fname+' '+au_lname from authors
 --authors table to print title name, Publisher's name, author's full name with quantity ordered and price for the order for all orders,
 --print first 5 orders after sorting them based on the price of order
 
-select top 5 t.title,a.au_fname,p.pub_name,t.price*s.qty 'Total Price' ,s.qty  from titles t 
-join titleauthor ta on t.title_id=ta.title_id
-join authors a on a.au_id=ta.au_id
-join sales s on s.title_id=t.title_id 
-join publishers p on p.pub_id=t.pub_id order by t.price*s.qty desc;
+--select sum(qty) from sales where title_id='PS2091'
+--select t.title_id, t.title,STRING_AGG(Concat(a.au_fname ,' ', a.au_fname),', ') 'Author Name' ,sum(s.qty)*max(t.price),sum(s.qty),max(price),min(price) 
+--from titleauthor ta 
+--join authors a on a.au_id=ta.au_id
+--join titles t on t.title_id=ta.title_id
+--join sales s on s.title_id=t.title_id
+--group by s.ord_num, t.title_id,t.title order by sum(s.qty)*max(t.price) desc;
+
+--select s.ord_num, t.title,a.au_fname+' '+ a.au_lname 'fullname',p.pub_name,t.price*s.qty 'Total Price' ,s.qty  from titles t 
+--join titleauthor ta on t.title_id=ta.title_id
+--join authors a on a.au_id=ta.au_id
+--join sales s on s.title_id=t.title_id 
+--join publishers p on p.pub_id=t.pub_id where t.title='Is Anger the Enemy?' order by t.price*s.qty desc;
+
+SELECT
+    aggregated.title_id,
+    aggregated.title,
+    aggregated.Author_Name,
+	max(aggregated.prize),
+	max(aggregated.prize)*sum(sales.qty)
+   
+FROM
+    (
+        SELECT
+            t.title_id,
+            t.title,
+            STRING_AGG(CONCAT(a.au_fname, ' ', a.au_lname), ', ') AS Author_Name,
+            t.price AS prize
+        FROM
+            titles t
+            JOIN titleauthor ta ON t.title_id = ta.title_id
+            JOIN authors a ON ta.au_id = a.au_id
+        GROUP BY
+            t.title_id,
+            t.title,
+			t.price
+    ) AS aggregated
+JOIN
+    sales ON aggregated.title_id = sales.title_id
+GROUP BY sales.title_id ,aggregated.prize,aggregated.Author_Name,aggregated.title,aggregated.title_id 
+order by max(aggregated.prize)*sum(sales.qty) desc;
+
+select t1.title, p.pub_name, t1.Name, t.price, sum(s.qty)'Quantity', sum(t.price*s.qty)'Total Price' from
+(select t.title_id, t.title, STRING_AGG(Concat(a.au_fname ,' ', a.au_fname),', ') 'Name' from titles t join titleauthor ta on ta.title_id = t.title_id 
+join authors a on ta.au_id=a.au_id group by t.title,t.title_id) as t1 join titles t on t1.title_id=t.title_id
+join sales s on s.title_id = t.title_id
+join publishers p on t.pub_id=p.pub_id
+group by t1.title,p.pub_name,t1.Name,t.price order by sum(t.price*s.qty) desc;
+
 
 
  
